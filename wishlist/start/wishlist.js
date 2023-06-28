46e45a990f74606c6967893722331383b9d61703
Frank Merema <frank.merema@rabobank.nl>
2023-06-28 11:45:38 +0200

    Implementation of assignment 2

--- wishlist/start/wishlist.js
+++ wishlist/start/wishlist.js
@@ -1,82 +1,126 @@
-const Wishlist = function (elementId) {
-    // This array will contain the items present in the wishlist
-    this.items = [];
-
-    // We keep a reference to the element associated to the wishlist
-    this.element = document.getElementById(elementId);
-};
-
-// TO IMPLEMENT
-Wishlist.prototype.contains = function (itemId) {
-    // This method should check in the `items` array if there's any item with the specified `itemId`, if yes, it should return true, otherwise it will return false;
-    return this.items.filter(function (item) {
-        return item.id === itemId;
-    }).length > 0;
-
-    // This is the same as above but written with an arrow function
-    // return this.items.filter(item => item.id === itemId).length > 0;
-};
-
-// TO IMPLEMENT
-Wishlist.prototype.add = function (item) {
-    // This method should add the item to the `items` array, then call the `render` method. You need to make sure that the item can't be added twice.
-    if (!this.contains(item.id)) {
-        this.items.push(item);
-        this.render();
+class WishList {
+    constructor(elementId) {
+        this.element = document.getElementById(elementId);
+        this.items = [];
     }
-};
 
-// TO IMPLEMENT
-Wishlist.prototype.remove = function (itemId) {
-    // This method should remove the item specified by `itemId` from the `items` array. Then, you need to call the render method
-    const index = this.items.findIndex(function (item) {
-        return item.id === itemId;
-    });
-
-    // This is the same as above but written with an arrow function
-    // const index = this.items.findIndex(item => item.id === itemId);
-
-    if (index >= 0) {
-        this.items.splice(index, 1);
-        this.render();
+    contains(itemId) {
+        // This method should check in the `items` array if there's any item with the specified `itemId`, if yes, it should return true, otherwise it will return false;
+        return this.items.filter(item => item.id === itemId).length > 0;
     }
-};
 
-Wishlist.prototype.render = function () {
-    this.clear();
+    add(item) {
+        // This method should add the item to the `items` array, then call the `render` method. You need to make sure that the item can't be added twice.
+        if (!this.contains(item.id)) {
+            this.items.push(item);
+            this.render();
+        }
+    }
 
-    this.items.forEach((i) => {
-        const li = document.createElement('li');
-        const remove = document.createElement('button');
-        remove.innerHTML = 'Remove from wishlist';
-        remove.addEventListener('click', () => {
-            this.remove(i.id);
-        });
-        li.innerHTML = i.name;
-        li.appendChild(remove);
+    remove(itemId) {
+        // This method should remove the item specified by `itemId` from the `items` array. Then, you need to call the render method
+        const index = this.items.findIndex(item => item.id === itemId);
 
-        this.element.appendChild(li);
-    });
-};
+        if (index >= 0) {
+            this.items.splice(index, 1);
+            this.render();
+        }
+    }
 
-Wishlist.prototype.clear = function () {
-    this.element
-        .querySelectorAll('li')
-        .forEach((li) => {
-            this.element.removeChild(li);
-        });
-};
+    render() {
+        this.clear();
 
-const wishlist = new Wishlist('wishlist');
-
-document
-    .querySelectorAll('.add-to-wishlist')
-    .forEach((e) => {
-        e.addEventListener('click', (ev) => {
-            const element = ev.target;
-            wishlist.add({
-                id: element.dataset.id,
-                name: element.dataset.name
+        this.items.forEach((i) => {
+            const li = document.createElement('li');
+            const remove = document.createElement('button');
+            remove.innerHTML = 'Remove from wishlist';
+            remove.addEventListener('click', () => {
+                this.remove(i.id);
             });
+            li.innerHTML = i.name;
+            li.appendChild(remove);
+
+            this.element.appendChild(li);
         });
+    }
+
+    clear() {
+        this.element
+            .querySelectorAll('li')
+            .forEach((li) => {
+                this.element.removeChild(li);
+            });
+    }
+}
+
+class ProductList {
+    constructor(elementId) {
+        this.element = document.getElementById(elementId);
+        this.products = [];
+    }
+
+    contains(productId) {
+        // This method should check in the `items` array if there's any item with the specified `itemId`, if yes, it should return true, otherwise it will return false;
+        return this.products.filter(product => product.id === productId).length > 0;
+    }
+
+    add(product) {
+        // This method should add the item to the `items` array, then call the `render` method. You need to make sure that the item can't be added twice.
+        if (!this.contains(product.id)) {
+            this.products.push(product);
+            this.render();
+        }
+    }
+
+    remove(productId) {
+        // This method should remove the item specified by `itemId` from the `items` array. Then, you need to call the render method
+        const index = this.products.findIndex(product => product.id === productId);
+
+        if (index >= 0) {
+            this.products.splice(index, 1);
+            this.render();
+        }
+    }
+
+    render() {
+        this.clear();
+
+        this.products.forEach(p => {
+            const li = document.createElement('li');
+            const add = document.createElement('button');
+            add.innerHTML = 'Add to wishlist';
+            add.dataset.id = p.id;
+            add.dataset.name = p.name;
+            add.addEventListener('click', (event) => {
+                const element = event.target;
+                wishList.add({
+                    id: element.dataset.id,
+                    name: element.dataset.name
+                });
+            });
+            li.innerHTML = p.name;
+            li.appendChild(add);
+
+            this.element.appendChild(li);
+        });
+    }
+
+    clear() {
+        this.element
+            .querySelectorAll('li')
+            .forEach((li) => {
+                this.element.removeChild(li);
+            });
+    }
+
+}
+
+const wishList = new WishList('wishList');
+const productList = new ProductList('productList');
+
+for (let i = 1; i <= 15; i++) {
+    productList.add({
+        id: `P${i}`,
+        name: `Product ${i}`
     });
+}
